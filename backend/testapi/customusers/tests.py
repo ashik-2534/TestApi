60d2f3bffd067bd1a3f74259592296a7350ef7a0
Ashik_Ridwan <ridwanashik004@gmail.com>
2025-09-20 21:55:39 +0600

    phase 1 complete

--- backend/testapi/customusers/tests.py
+++ backend/testapi/customusers/tests.py
@@ -12,7 +12,7 @@ from datetime import datetime
 
 
 class APIClient:
-    def __init__(self, base_url="http://127.0.0.1:8000/api"):
+    def __init__(self, base_url="http://127.0.0.1:8000/api/users"):
         self.base_url = base_url
         self.access_token = None
         self.refresh_token = None
